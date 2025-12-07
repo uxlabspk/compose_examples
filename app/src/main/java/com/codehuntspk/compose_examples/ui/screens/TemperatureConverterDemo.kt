18d7e3c8bf21141ee8aa45e982c5e56fe8fa9034
Muhammad Naveed <naveed@codehuntspk.com>
2025-12-07 11:05:53 +0500

    feat: Add TemperatureConverterDemo screen.

---
+++ app/src/main/java/com/codehuntspk/compose_examples/ui/screens/TemperatureConverterDemo.kt
@@ -0,0 +1,21 @@
+package com.codehuntspk.compose_examples.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TemperatureConverterDemo() {
+
+}
+
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    widthDp = 300,
+    heightDp = 700
+)
+@Composable
+fun TemperatureConverterDemoPreview() {
+
+}
